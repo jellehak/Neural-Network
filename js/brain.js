34135991e98fab676f980e5cd753a11bf69e131c
Jelle <jellehak@gmail.com>
2021-11-25 17:14:32 +0100

    cleaner NeuralNetwork.update

---
+++ js/brain.js
@@ -0,0 +1,13 @@
+    this.allNeurons.forEach(n => {
+      if (this.allSignals.length < this.currentMaxSignals - this.maxConnectionPerNeuron) {
+        // currentMaxSignals - maxConnectionPerNeuron because allSignals can not bigger than particlePool size
+        // if (n.recievedSignal && (currentTime - n.lastSignalRelease > n.releaseDelay) && n.firedCount < 8) { // Random mode
+        // if (n.recievedSignal && !n.fired) { // Single propagation mode
+    })
+      // console.log('Spark')
+      this.allNeurons.forEach(n => {
+      })
+    this.allSignals.forEach((s, index) => {
+
+        this.allSignals.splice(index, 1)
+    })
